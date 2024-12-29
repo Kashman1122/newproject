54c6978a7e047cb60894e5cb4879fa3e03c21def
Kashish <134233339+Kashman1122@users.noreply.github.com>
2024-12-29 15:11:05 +0530

    settings.py

--- DatasetProvider/DatasetProvider/settings.py
+++ DatasetProvider/DatasetProvider/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lo78lui9%@8tmymp6xt(evk*mzfq7j3ccq5xx+8+p$o5wy3lj@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.84.65']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
